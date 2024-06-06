5f7105aa23b7d5d05e3420221a551191d22d14a5
arunachintala <aruanchintala144@gmail.com>
2024-06-06 16:55:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Zippyy from './Components/Zippyy/Zippyy';
+function App() {
+    return (
+        <div>
+            <Zippyy />
+        </div>
+    )
+}
+export default App;
